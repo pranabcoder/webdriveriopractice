d3f904e28e94077ccad4d6d7589557550c3661fc
Pranab Ghosh <pranabit6@gmail.com>
2024-01-30 23:48:41 +0530

    added Lines in basics2.js file

--- JsFundamentals/basics2.js
+++ JsFundamentals/basics2.js
@@ -14,6 +14,21 @@ if (flag) {
 if (!flag) {
     console.log("Condition Satisfied");
 } else {
+    console.log(flag);
     console.log("Condition Not Satisfied");
 }
 
+/*
+Example - While Loop
+
+while (true) {
+    console.log("I am in while loop");
+}
+*/
+let i = 0;
+let output = "";
+while (i < 10) {
+    output += i + "\t";
+    i++;
+}
+console.log(output); // 0 1 2 3 4 5 6 7 8 9
