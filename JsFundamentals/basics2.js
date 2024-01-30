811bcdaad57f021b0b1b1b4fe978e4d02035feec
Pranab Ghosh <pranabit6@gmail.com>
2024-01-30 22:28:27 +0530

    Added line in basics2.js file

---
+++ JsFundamentals/basics2.js
@@ -0,0 +1,19 @@
+/*
+Example - Conditional Statements
+ */
+
+const flag = true;
+if (flag) {
+    console.log("Flag is true");
+} else if (!flag) {
+    console.log("Flag is false");
+} else {
+    console.log("Flag is not a boolean");
+}
+
+if (!flag) {
+    console.log("Condition Satisfied");
+} else {
+    console.log("Condition Not Satisfied");
+}
+
