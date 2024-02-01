fbfbe759c3cadcfeb5b1d245971cca477a303da5
Pranab Ghosh <pranabit6@gmail.com>
2024-02-01 13:28:19 +0530

    Added lines in basics3.js file

--- JsFundamentals/basics3.js
+++ JsFundamentals/basics3.js
@@ -46,5 +46,35 @@ Example - Add new element to the beginning of an array
 marks.unshift(12);
 console.log(marks); // [ 12, 20, 40, 35, 14, 37, 100 ]
 
+/*
+Example - Show the index number of an array element
+*/
+console.log(marks.indexOf(35)); // 3
 
+/*
+Example - Check an element is present in an array or not
+*/
+console.log('ELement present in the array or not', marks.includes(35)); // true
+
+/*
+Example - to create a new array from a section of an existing array
+*/
+let newMarks = marks.slice(2, 5);
+console.log('to create a new array from a section of an existing array', newMarks); // [ 40, 35, 14 ]
+
+/*
+Example - Print all the elements of an array
+*/
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i]);
+}
+
+/*
+Example - To Sum up all the elements of an array
+*/
+let sum = 0;
+for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+}
+console.log('Sum of all the elements of an array', sum); // 258
 
