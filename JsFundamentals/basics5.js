0fdd974f4582915791588a5acf133f6e1efbf46d
Pranab Ghosh <pranabit6@gmail.com>
2024-02-02 16:32:45 +0530

    added lines in basics5.js file

---
+++ JsFundamentals/basics5.js
@@ -0,0 +1,29 @@
+// String Example 
+
+let day = "Monday ";
+
+/*
+Example - Get String length
+*/
+console.log('Check the length of a string', day.length); // 7
+
+/*
+Example - Get Substring from a string
+*/
+console.log('Get a substring from a string', day.slice(0, 3)); // Mon
+
+/*
+Example - Get the character at a specific position
+*/
+console.log('Get the character from a string ', day[2]); // n
+
+/* 
+Example - Split the string into an array
+*/
+let splitDay = day.split('n');
+console.log('Split the string into an array', splitDay); // [ 'Mon', 'day ' ]
+
+/* 
+Example - Print the splited array elements
+*/
+console.log('Print the splited array elements =>', splitDay[1]); // day
