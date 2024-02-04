fb5424d92d194dbfde49d8c1d0254f150e7f6b16
Pranab Ghosh <pranabit6@gmail.com>
2024-02-04 13:12:08 +0530

    added lines in basics7.js and basics8.js

--- JsFundamentals/basics8.js
+++ JsFundamentals/basics8.js
@@ -2,3 +2,18 @@ import Guest from './basics7.js';
 
 let guest = new Guest("John", "Doe");
 console.log('Full name of the guest is', guest.fullName()); // Full name of the guest is John Doe
+
+/*
+Example Inheritance
+*/
+
+class Pet extends Guest {
+    constructor(firstName, lastName, petName) {
+        super(firstName, lastName);
+        this.petName = petName;
+    }
+
+    petInfo() {
+        return `Pet name is ${this.petName}`;
+    }
+}
