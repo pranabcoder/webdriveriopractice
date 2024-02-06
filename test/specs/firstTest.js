65fd600ce89325d9dae925c981cc22c47f42448f
Pranab Ghosh <pranabit6@gmail.com>
2024-02-07 00:23:46 +0530

    added lines in firstTest.js

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -4,5 +4,25 @@ describe('E-Commerce Application', async() => {
         await browser.url('https://rahulshettyacademy.com/loginpagePractise/');
         await browser.maximizeWindow();
         console.log(await browser.getTitle());
+        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy');
+        /*
+        await $("#username").setValue("rahulshettyacademy");
+        await browser.pause(3000);
+        const passwordLocator = $("//input[@type='password']");
+        await browser.pause(3000);
+        await passwordLocator.setValue("learning");
+        const signingButton = $("#signInBtn");
+        await signingButton.click();
+        await browser.pause(3000);
+        */
+        await $("#username").setValue("secondUser");
+        await browser.pause(3000);
+        const passwordLocator = $("//input[@type='password']");
+        await browser.pause(3000);
+        await passwordLocator.setValue("learning");
+        const signingButton = $("#signInBtn");
+        await signingButton.click();
+        await browser.pause(3000);
+        console.log(await $(".alert-danger").getText());
     });
 });
