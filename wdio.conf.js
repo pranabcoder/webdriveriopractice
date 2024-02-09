17e3f351ec054401493a3d21fa1a8b00c4e0dae5
Pranab Ghosh <pranabit6@gmail.com>
2024-02-10 00:25:03 +0530

    updated wdio.conf.js file

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,7 +50,7 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
     }],
 
     //
@@ -100,7 +100,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    // services: ['selenium-standalone'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
